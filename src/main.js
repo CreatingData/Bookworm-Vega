69b41aa8bab91927bdfb4ded5083445811ef9a8a
Ben Schmidt <bmschmidt@gmail.com>
2019-03-31 10:20:13 -0400

    webpage stability

--- src/main.js
+++ src/main.js
@@ -33,206 +33,245 @@ const counttypes = {
 const definitions = {};
 
 export default class Bookworm {
-    
-    constructor(selector, width = 600, height = 400) {
-        this.selector = selector;
-        this.width = width;
-        this.height = height;
-        this.history = [null];
-        this.previous_aes = undefined;
-        this.query = { 'search_limits': {} };
-        this.data = [];
-    }
-    
-    buildSpec(query) {
-        const { data, width, height } = this;
-        const type = query.plottype;
-        this.spec = new plots[type](query)
-            .data(this.smooth(data))
-            .spec();
-        this.spec = merge(this.spec, this.query.vega || {})
-        this.spec.width = +width;
-        this.spec.height = +height;
-    }
 
-    smooth() {
-        const { query, data } = this
-        if (query.smoothingSpan) {
-            return smooth(
-                data,
-                query.smoothingSpan,
-                query.aesthetic.x,
-                query.counttype,
-                "faussian",
-                false)
-        } else {
-            return data
-        }
+  constructor(selector, width = 600, height = 400) {
+    this.selector = selector;
+    this.width = width;
+    this.height = height;
+    this.history = [null];
+    this.previous_aes = undefined;
+    this.query = { 'search_limits': {} };
+    this.data = [];
+  }
+
+  buildSpec(query) {
+    const { data, width, height } = this;
+    const type = query.plottype;
+    this.spec = new plots[type](query)
+      .data(this.smooth(data))
+      .spec();
+    this.spec = merge(this.spec, this.query.vega || {})
+    this.spec.width = +width;
+    this.spec.height = +height;
+  }
+
+  smooth() {
+    const { query, data } = this
+    if (query.smoothingSpan) {
+      return smooth(
+        data,
+        query.smoothingSpan,
+        query.aesthetic.x,
+        query.counttype,
+        "faussian",
+        false)
+    } else {
+      return data
     }
+  }
 
-    render() {
-        return vegaEmbed(this.selector, this.spec)
-    }
+  render() {
+    return vegaEmbed(this.selector, this.spec)
+  }
 
-    interpolateSpecs() {
-        const d = 5000
-        const interpolator = interpolate(this.history[1].data, this.spec.data)
+  interpolateSpecs() {
+    const d = 5000
+    const interpolator = interpolate(this.history[1].data, this.spec.data)
 
-        this.t = timer (elapsed => {
-            let ratio = elapsed/d
-            if (elapsed/d > 1) {ratio = 1}
-            this.spec.data = interpolator(ratio)
-            this.render()
-            if (ratio === 1) {
-                this.t.stop()
-            }
+    this.t = timer (elapsed => {
+      let ratio = elapsed/d
+      if (elapsed/d > 1) {ratio = 1}
+      this.spec.data = interpolator(ratio)
+      this.render()
+      if (ratio === 1) {
+        this.t.stop()
+      }
+    })
+
+  }
+
+  plotAPI(query, drawing = true) {
+    this.query = alignAesthetic(query)
+    if (drawing) {
+      return bookwormFetch(query)
+        .then(data => {
+          this.data = data
+          this.buildSpec(query)
+          this.history = [this.spec, this.history[0]]
+          return this.render()
         })
-        
     }
-    
-    plotAPI(query, drawing = true) {
-        this.query = alignAesthetic(query)
-        if (drawing) {
-            return bookwormFetch(query)
-              .then(data => {
-                this.data = data
-                this.buildSpec(query)
-                this.history = [this.spec, this.history[0]]
-                return this.render()
-          })
-        }
-    }
-    
+  }
+
 }
 
 function validate(query) {
-    // Throw errors on bad queries.
-    if (!query.plottype) {throw("Must have a 'plottype' key")};
+  // Throw errors on bad queries.
+  // currently cursory
+  if (!query.plottype) {throw("Must have a 'plottype' key")};
 }
 
 function bookwormFetch(query) {
-    let domain = query.host || 'https://bookworm.htrc.illinois.edu'
-    const newQuery = alignAesthetic(query);
-    newQuery.method = newQuery.method || "data"
-    newQuery.format = newQuery.format || "json"
+  let domain = query.host || 'https://bookworm.htrc.illinois.edu'
+  const newQuery = alignAesthetic(query);
+  newQuery.method = newQuery.method || "data"
+  newQuery.format = newQuery.format || "json_c"
 
-    const url = encodeURI(JSON.stringify(newQuery));
-    // This wrapper should only apply in an emergency, but currently
-    // is always used.
-    if (domain.startsWith("http:")) {
-        //    domain = "https://cors-anywhere.herokuapp.com/" +  domain
-    }
-    
-    return d3Fetch
-      .json(`${domain}/cgi-bin/dbbindings.py?query=${url}`)
-      .then( (data) => parseBookwormData(data.data, query))
+  const url = encodeURI(JSON.stringify(newQuery));
+  // This wrapper should only apply in an emergency, but currently
+  // is always used.
+  if (domain.startsWith("http:")) {
+    //    domain = "https://cors-anywhere.herokuapp.com/" +  domain
+  }
+
+  return d3Fetch
+    .json(`${domain}/cgi-bin/dbbindings.py?query=${url}`)
+    .then( (data) => {
+      const results = parseBookwormData(data.data, query)
+      if (results[0]['Search'] !== undefined) {
+        // More informative search labels.
+        const [baseq, search_labels] = labels(query.search_limits)
+        results.forEach(d => {
+          d.Search = search_labels[d.Search]
+        })
+      }
+      return results
+    })
 }
 
 
 function alignAesthetic(query) {
-    // Percolate aesthetic to 'counttypes' or 'groups' as necessary.
-    query.groups = [];
-    query.counttype = [];
-    if (query.aesthetic) {
-        keys(query.aesthetic).forEach( (key) => {
-            const val = query.aesthetic[key]
-            if (val === 'search_limits' || val === 'Search') return false
-            if (counttypes[val] !== undefined) {
-                query.counttype.push(val)
-            } else {
-                query.groups.push(val)
-            }
-        })
-    }
-    return query
+  // Percolate aesthetic to 'counttypes' or 'groups' as necessary.
+  query.groups = [];
+  query.counttype = [];
+  if (query.aesthetic) {
+    keys(query.aesthetic).forEach( (key) => {
+      const val = query.aesthetic[key]
+      if (val === 'search_limits' || val === 'Search') return false
+      if (counttypes[val] !== undefined) {
+        query.counttype.push(val)
+      } else {
+        query.groups.push(val)
+      }
+    })
+  }
+  return query
 }
 
 function serverSideJSON(queryFull) {
-    //returns only the query elements that actually matter to the server.
-    //useful for seeing if the query needs to be rerun, or if all changes
-    //can be handled client-side.
+  //returns only the query elements that actually matter to the server.
+  //useful for seeing if the query needs to be rerun, or if all changes
+  //can be handled client-side.
 
-    var query = JSON.parse(JSON.stringify(queryFull))
-        delete(query.aesthetic)
-        delete(query.scaleType)
-        delete(query.smoothingSpan)
-        delete(query.plottype)
-        return JSON.stringify(query);
+  var query = JSON.parse(JSON.stringify(queryFull))
+  delete(query.aesthetic)
+  delete(query.scaleType)
+  delete(query.smoothingSpan)
+  delete(query.plottype)
+  delete(query.host)
+  return JSON.stringify(query);
 
 }
 
-function parseBookwormData(json,locQuery) {
-    // Changes the shape of the hierarchical json the API
-    // delivers to a flat one with attribute names
-    // which takes more space but plays more nicely with d3/javascript.
-    var names = []
-    var bookworm = this
+function parseBookwormData(data,locQuery) {
+  var names = []
+  var bookworm = this
 
-    if (json instanceof Array) {
-        // when the return is an array, more than one search
-        // limit has been entered.
-        const [ baseq, diffs ] = labels(locQuery.search_limits)
-        console.log(diffs)
-        const newOut = {};
-        json.forEach( (d, i) => {
-            newOut[diffs[i]] = d
-        })
-        json = newOut;
-        names.push("Search")
-    }
+  if (data instanceof Array) {
 
-    names = names
-      .concat(locQuery.groups)
-      .concat(locQuery.counttype);
+    const [baseq, labels] = labels(locQuery.search_limits)
+    // You can define multiple searches;
+    // Those are passed to a new index called 'search.'
+    const multiples = data.map(parseBookwormData)
+    
+    multiples.map((search, i) => {
+      search.forEach(row => {
+        row['Search'] = i
+      })
+    })
+    return multiples.reduce( (a, b) => a.concat(b))
 
-    function flatten(hash,prepend) {
-        prepend = prepend || [];
-        var results = Object.keys(hash).map(function(key) {
-            var newpend = prepend.concat(key)
-                if (hash[key] instanceof Array)
-            {
-                return(newpend.concat(hash[key]))
-            }
-            else {
-                var vals = flatten(hash[key],newpend)
-                    //is this doing anything different from return (vals)?
-                    return(
-                        vals.map(function(array) {
-                            return(array)
-                        })
-                    )
-            }
-        })
+  }
 
-            if (results[0][0] instanceof Array) {
-                return(results.reduce(function(a,b){return(a.concat(b))}))
-            } else {
-                return(results)
-            }
-    }
+  
+  const keyz = keys(data)
 
-    function toObject(names, values) {
-        var result = {};
-        for (var i = 0; i < names.length; i++) {
-            if (names[i].endsWith("_year")) {
-                values[i] = `${parseInt(values[i])}`
-            }
-            result[names[i]] = values[i];
-        }
-        return result;
-    };
+  const columns = keyz.map(
+    k => data[k].map(
+      // Test by properties if it's an array 
+      d => d.reduceRight ?
+        // If so, unroll it using the first (integer) key as the run-length.
+        Array(d[0] + 1).fill(d[1]):
+        // otherwise, 
+        d
+    )
+    // flatten out the nested lists.
+      .flat()
+  )
 
-    //run flatten initially with nothing prepended: as it recurses, that will get filled in.
-    try {
-        var flat = flatten(json);
-    } catch(err) {
-        var flat = []
-    }
-    //add the labels.
-    var results = flat.map(function(localdata){
-        return(toObject(names,localdata));
+  let results = columns[0]
+    .map((x, i) => {
+      const row = {};
+      keyz.forEach((k, j) => {
+        row[k] = columns[j][i]
+      })
+      return row;
     })
 
-    return(results)
+
+  results = add_href(results, locQuery)
+
+  return results
+}
+
+
+function add_href(data, locQuery) {
+  
+  const query = JSON.parse(serverSideJSON(locQuery))
+  
+  query['method'] = 'search';
+  query['format'] = 'html';
+  
+  return data
+    .map(row => {
+      
+      // This could be more dynamic; currently, just
+      // open a search in a new window.
+      query.groups.forEach( g => {
+        query['search_limits'][g] = [row[g]]
+      })
+      
+      const query_string = encodeURI(JSON.stringify(query))
+      
+      row['href'] = `${locQuery.host}/cgi-bin/dbbindings.py?${query_string}`
+      
+      return row
+    })
+  return results
+}
+
+function toObject(names, values) {
+  // A hack against the way the json format
+  // loses type information.
+  var result = {};
+  for (var i = 0; i < names.length; i++) {
+    if (names[i].endsWith("_year") || names[i].endsWith("_month")) {
+      values[i] = `${parseInt(values[i])}`
+    }
+    result[names[i]] = values[i];
+  }
+  return result;
+};
+
+//run flatten initially with nothing prepended: as it recurses, that will get filled in.
+try {
+  var flat = flatten(json);
+} catch(err) {
+  var flat = []
+}
+
+window.bookworm_search = function(json) {
+
+  //add the labels.
 
 }
--- src/translators.js
+++ src/translators.js
@@ -2,232 +2,235 @@ import { keys } from 'd3-collection';
 import { extend } from 'lodash-es';
 import labels from './search_limit_labels';
 
-// Translators 
+// Translators
 
 class Translator {
-    /* 
-       A translator takes a bookworm query and returns a
-       vega-lite spec.
-       
-       The most important methods is 'translate', which builds the spec.
-       This is not defined for the 
-     */
-    constructor(query) {
-      // The bookworm query
-      this.query = query;
-      // The vega spec.
-      this.p = {
-        encoding: {},
+  /*
+    A translator takes a bookworm query and returns a
+    vega-lite spec.
+
+    The most important methods is 'translate', which builds the spec.
+
+    This is undefined in the base class.
+
+  */
+  constructor(query) {
+    // The bookworm query
+    this.query = query;
+    // The vega spec.
+    this.p = {
+      encoding: {},
+    }
+  }
+
+  make_titles() {
+    if (this.query.search_limits.length) {
+      const [ baseq, diffs ] = labels(this.query.search_limits)
+      this.p.title = {
+        "orient": "bottom",
+        "text": "Limits: " + baseq
       }
     }
+  }
 
-    make_titles() {
-        if (this.query.search_limits.length) {
-            const [ baseq, diffs ] = labels(this.query.search_limits)
-            this.p.title = {
-                "orient": "bottom",
-                "text": "Limits: " + baseq
-            }
-        }
+  spec(val) {
+    const { data } = this;
+    if (val === undefined) {
+      this.translate()
+      this.make_titles()
+      return this.p
+    } else {
+      this.p = val
     }
-    
-    spec(val) {
-        const { data } = this;
-        if (val === undefined) {
-            this.translate()
-            this.make_titles()
-            return this.p
-        } else {
-            this.p = val
-        }
-    }
+  }
 
-    translate() {
-        throw ("No translation defined for this class")
-    }
+  translate() {
+    throw ("No translation defined for this class")
+  }
 
-    data(data) {
-        // Attaches data to the query.
-        // Returns the translator
-        this.p.data = {values: data}
+  data(data) {
+    // Attaches data to the query.
+    // Returns the translator
+    this.p.data = {values: data}
 
-        return this
-    }
+    return this
+  }
 
-    aestheticize() {
-        const { p, query } = this;
+  aestheticize() {
+    const { p, query } = this;
 
-        // Updates a copy of the spec and returns it.
-        keys(query.aesthetic).forEach(k => {
-            
-            // Handled by the choropleth code.
-            if (k === 'state') {return}
-            const val = query.aesthetic[k]
-            p.encoding[k] = extend(p.encoding[k], { field: val })
-            p.encoding[k] = extend(p.encoding[k], correctTimes(val));
-        })
-    }
+    // Updates a copy of the spec and returns it.
+    keys(query.aesthetic).forEach(k => {
+
+      // Handled by the choropleth code.
+      if (k === 'state') {return}
+      const val = query.aesthetic[k]
+      p.encoding[k] = extend(p.encoding[k], { field: val })
+      p.encoding[k] = extend(p.encoding[k], correctTimes(val));
+      p.encoding['href'] = {'field': 'href', 'type': 'nominal'}
+    })
+  }
 
 }
 
 function correctTimes(f) {
-    const field = extractRelevantField(f)
-        if (field === 'year') {
-            return { type: "ordinal", timeUnit: "utcyear"}
-        } else {
-            return {}
-        }
+  const field = extractRelevantField(f)
+  if (field === 'year') {
+    return { type: "ordinal", timeUnit: "utcyear"}
+  } else {
+    return {}
+  }
 }
 
 
 var extractRelevantField = function(dateKey) {
-    var output = undefined
-    dateKey.split("_").reverse().forEach(function(phrase) {
-        //The first date phrase to appear is the one we're using;
-        //The reverse forEach means that's the one that will persist
-        if (['year','month','day','week','decade','century',"Year","Decade","yearchunk","hour"].indexOf(phrase) >=0) {output=phrase}
-    })
-        return output
+  var output = undefined
+  dateKey.split("_").reverse().forEach(function(phrase) {
+    //The first date phrase to appear is the one we're using;
+    //The reverse forEach means that's the one that will persist
+    if (['year','month','day','week','decade','century',"Year","Decade","yearchunk","hour"].indexOf(phrase) >= 0) {output=phrase}
+  })
+  return output
 }
 
 
 
 export class streamgraph extends Translator {
-    translate() {
-        this.p = extend(this.p, {
-            "mark": "area",
-            "encoding": {
-                x: {
-                  "axis": {"domain": false, "tickSize": 0}
-                },
-                y: {"stack": "center"},
-                color: {type: "nominal"}
-            }
-        })
-        this.aestheticize()
-        return this.p
-    }
+  translate() {
+    this.p = extend(this.p, {
+      "mark": "area",
+      "encoding": {
+        x: {
+          "axis": {"domain": false, "tickSize": 0}
+        },
+        y: {"stack": "center"},
+        color: {type: "nominal"}
+      }
+    })
+    this.aestheticize()
+    return this.p
+  }
 }
 
 
 
 export class USchoropleth extends Translator {
 
-    // unusually, I have to overwrite the data method, b/c it goes some
-    // where else.
+  // unusually, I have to overwrite the data method, b/c it goes some
+  // where else.
 
-    data(data) {
-        const { state, color, row, column } = this.query.aesthetic;
-        const to_match = [color, row, column]
-        data.forEach(d => {
-            d[state] = d[state].replace(" (State)", "")
-        })
-        this.p.transform = [{
-            "lookup": "properties.NAME",
-            "from": {
-                "data": { "values": data },
-                "key": state,
-                "fields": to_match.filter(d => d) // (No undefined)
-            }
-        }]
-        return this
-    }
-    
-    translate() {
+  data(data) {
+    const { state, color, row, column } = this.query.aesthetic;
+    const to_match = [color, row, column]
+    data.forEach(d => {
+      d[state] = d[state].replace(" (State)", "")
+    })
+    this.p.transform = [{
+      "lookup": "properties.NAME",
+      "from": {
+        "data": { "values": data },
+        "key": state,
+        "fields": to_match.filter(d => d) // (No undefined)
+      }
+    }]
+    return this
+  }
+
+  translate() {
     this.p = extend(this.p, {
-        "data": {
-            "url": "/data/States.topojson",
-            "format": {
-                "type": "topojson",
-                "feature": "-"
-            }
-        },
-        "projection": {
-            "type": "albersUsa"
-        },
-        "mark": "geoshape",
-        "encoding": {
-            "color": {
-                "type":"quantitative"
-            }
+      "data": {
+        "url": "/data/States.topojson",
+        "format": {
+          "type": "topojson",
+          "feature": "-"
         }
+      },
+      "projection": {
+        "type": "albersUsa"
+      },
+      "mark": "geoshape",
+      "encoding": {
+        "color": {
+          "type":"quantitative"
+        }
+      }
     })
     this.aestheticize()
     return this.p
-        
-    }
+
+  }
 }
-        
+
 export class heatmap extends Translator {
-    translate() {
-        const { query } = this;
-        this.p = extend(this.p, {
-            "mark": {"type": "rect"},
-            "encoding": {
-                color: {"type": "quantitative"},
-                x: {"type": "ordinal"},
-                y: {"type": "ordinal"}
-            }
-        })
-            this.aestheticize()
-            return this.p
-     }
+  translate() {
+    const { query } = this;
+    this.p = extend(this.p, {
+      "mark": {"type": "rect"},
+      "encoding": {
+        color: {"type": "quantitative"},
+        x: {"type": "ordinal"},
+        y: {"type": "ordinal"}
+      }
+    })
+    this.aestheticize()
+    return this.p
+  }
 
 }
 
 export class barchart extends Translator {
-    
-    translate() {
-        
-        const { query, p } = this;
-        
-        this.p = extend(this.p, {
-            "mark": {"type": "rect"},
-            "encoding": {
-                x: {"type": "quantitative"},
-                y: {"type": "ordinal"},
-            }
-        })
-        
-        this.aestheticize()
-            
-        return this.p
-    }
+
+  translate() {
+
+    const { query, p } = this;
+
+    this.p = extend(this.p, {
+      "mark": {"type": "rect"},
+      "encoding": {
+        x: {"type": "quantitative"},
+        y: {"type": "ordinal"},
+      }
+    })
+
+    this.aestheticize()
+
+    return this.p
+  }
 }
 
 
 export class linechart extends Translator {
-    translate() {
-        const { query, p } = this;
-        this.p = extend(this.p, {
-            "mark": {"type": "line"},
-            "encoding": {
-                x: {"type": "quantitative"},
-                y: {"type": "quantitative"},
+  translate() {
+    const { query, p } = this;
+    this.p = extend(this.p, {
+      "mark": {"type": "line"},
+      "encoding": {
+        x: {"type": "quantitative"},
+        y: {"type": "quantitative"},
 
-            }
-        })
-        this.aestheticize()
-        return this.p
-    }
+      }
+    })
+    this.aestheticize()
+    return this.p
+  }
 }
 
 export class pointchart extends Translator {
 
-    translate() {
-        const { query, p } = this;
-        this.p = extend(this.p, {
-            "mark": {"type": "circle","size":120},
-            "encoding": {
-                "y": {
-                    // Baked in: we sort descending.
-                    "type": "nominal",
-                    "sort":{"op":"mean", "field": query.aesthetic.x, "order":"descending"}
-                },
+  translate() {
+    const { query, p } = this;
+    this.p = extend(this.p, {
+      "mark": {"type": "circle","size":120},
+      "encoding": {
+        "y": {
+          // Baked in: we sort descending.
+          "type": "nominal",
+          "sort":{"op":"mean", "field": query.aesthetic.x, "order":"descending"}
+        },
 
-            }
-        })
-        this.aestheticize()
-            return this.p
-    }
+      }
+    })
+    this.aestheticize()
+    return this.p
+  }
 }
